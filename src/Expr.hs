d78cf578b19cd0b7bd19e350cf4cd9521d517594
Kasco01 <73853806+Kasco01@users.noreply.github.com>
2022-03-28 01:51:39 +0100

    Add files via upload
    Working code :))
    
    https://www.youtube.com/watch?v=G7LJC9vJluU

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,104 +1,196 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Expr where
 
 import Data.Either
 
-import Parsing
-
 type Name = String
+type Msg = String
 
--- At first, 'Expr' contains only addition, conversion to strings, and integer
--- values. You will need to add other operations, and variables
-data Expr = Add Expr Expr
+data Expr = Val Value
+          | Var Name
+          -- math operation 
+          | Add Expr Expr
           | Sub Expr Expr
-          | Mult Expr Expr
+          | Mul Expr Expr
           | Div Expr Expr
-          | ToString Expr
-          | Val Int
-  deriving Show
+          | Abs Expr
+          | Mod Expr Expr
+          | Pow Expr Expr
+          -- string concat
+          | Concat Expr Expr
+
+          | Compare Expr Expr
+          | Eq Expr Expr
+          | NE Expr Expr
+          | Gt Expr Expr
+          | Lt Expr Expr
+          | GE Expr Expr
+          | LE Expr Expr
+
+          | FuncCallExpr Name [Expr] --Name is name of function
+          | InputExpr
+
+          | Not Expr
+          | And Expr Expr
+          | Or Expr Expr
+  deriving (Show, Eq)
 
 -- These are the REPL commands
 data Command = Set Name Expr -- assign an expression to a variable name
              | Print Expr    -- evaluate an expression and print the result
+             | While Expr [Command]
+             | IfE Expr [Command] [Command]
+             | If Expr [Command]
+             | Func Name [Name] [Command] -- Name -> name of function, [Name] -> Argument variables, [Command] -> Commands in the function
+             | VoidFuncCall Name [Expr]
+             | Return Expr
+             | Expr Expr
              | Quit
   deriving Show
 
-data Value = IntVal Int| FloatVal Float| StrVal String| BoolVal Bool|Null
+data Error = ExprErr Name Msg -- Name refers to the expression that causes the error, Msg is the error msg that will be print when the error occur
+  deriving Show
+
+data Value = IntVal Int | FltVal Float | StrVal String | BoolVal Bool | FunCall Name [Expr]| Null | Input
   deriving Eq
 
-
-data Error = Nothing -- Not yet define
-
 instance Show Value where
-  show (IntVal i)      = show i
-  show (FloatVal f)    = show f
-  show (StrVal s)      = show s
-  show (BoolVal b)     = show b
-  show Null            = "NULL"
+  show (IntVal i)  = show i
+  show (FltVal f)  = show f
+  show (StrVal s)  = show s
+  show (BoolVal b) = show b
+  show Null        = "NULL"
+  show Input       = "INPUT"
 
-data BinTree = Leaf | Node (Name, Value) BinTree BinTree
+data BTree = Leaf | Node (Name, Value) BTree BTree
 
-instance Show BinTree where
-  show bt = show (showTree bt)
+searchBinTree :: Name -> BTree -> Either Error Value
+searchBinTree _name Leaf = Left (ExprErr "Var" (_name ++ " not found"))
+searchBinTree _name (Node (name, value) binTreeL binTreeR)
+  | _name < name = searchBinTree _name binTreeL
+  | _name > name = searchBinTree _name binTreeR
+  | otherwise    = Right value
 
-showTree :: BinTree -> [(Name, Value)]
-showTree Leaf                       = []
-showTree (Node (name, value) lt rt) = showTree lt ++ [(name, value)] ++ showTree rt
 
-eval :: [(Name, Value)] -> -- Variable name to value mapping
+eval :: BTree -> -- Variable name to value mapping
         Expr -> -- Expression to evaluate
-        Maybe Value -- Result (if no errors such as missing variables)
+        Either Error Value -- Result (if no errors such as missing variables)
+eval vars (Val x)      = Right x -- for values, just give the value directly
+eval vars (Var x)      = searchBinTree x vars
+-- string concatenatopm
+eval vars (Concat x y) = case (eval vars x, eval vars y) of
+  (Right (StrVal a), Right (StrVal b)) -> Right (StrVal (a ++ b))
+  (Right (StrVal a), Right not_string) -> Left (ExprErr "Concat" (show not_string ++ " is not a string"))
+  (Right (StrVal a), Left eval_err)    -> Left eval_err
+  (Right not_string, _)                -> Left (ExprErr "Concat" (show not_string ++ " is not a string"))
+  (Left eval_err, _)                   -> Left eval_err
+eval vars InputExpr         = Right Input
+eval vars (FuncCallExpr name args) = let toString :: [Expr] -> Either Error Value
+                                         toString [intExpression]  = case eval vars intExpression of
+                                                                          Right (IntVal i) -> Right (StrVal (show i))
+                                                                          Right (FltVal f) -> Right (StrVal (show f))
+                                                                          Right (StrVal s) -> Right (StrVal s)
+                                                                          _                -> Left (ExprErr "toString" (show intExpression ++ " cannot be converted to string"))
+                                         toInt :: [Expr] -> Either Error Value
+                                         toInt [strExpression]  = case eval vars strExpression of
+                                                                       Right (StrVal i) -> Right (IntVal (read i :: Int))
+                                                                       Right (FltVal i) -> Right (IntVal (round i))
+                                                                       _               ->  Left (ExprErr "toInt" (show args ++ " cannot be converted to integer"))
+                                         toFlt :: [Expr] -> Either Error Value
+                                         toFlt [strExpression]  = case eval vars strExpression of
+                                                                       Right (StrVal i) -> Right (FltVal (read i :: Float))
+                                                                       _               ->  Left (ExprErr "toFlt" (show args ++ " cannot be converted to float"))
 
--- need to redefine the following functions with bintree
-eval vars (Val x) = Just (IntVal(read (show x))) -- for values, just give the value directly
-eval vars (Add x y) = Just (IntVal((+) (read (show x)) (read (show y)))) -- return an error (because it's not implemented yet!)
-eval vars (Sub x y) = Just (IntVal((-) (read (show x)) (read (show y))))
-eval vars (Mult x y) = Just (IntVal((*) (read (show x)) (read (show y))))
-eval vars (Div x y) = Just (FloatVal((/) (read (show x)) (read (show y))))
-eval vars (ToString x) = Just (StrVal(show x))
+                                         in case name of
+                                                 "toString" -> toString args
+                                                 "toInt"    -> toInt args
+                                                 "toFloat"  -> toFlt args
+                                                 _          -> Right (FunCall name args)
 
-digitToInt :: Char -> Int
-digitToInt x = fromEnum x - fromEnum '0'
+eval vars (Abs x)             = case eval vars x of
+                                     Right (IntVal i) -> Right (IntVal (abs i))
+                                     Right (FltVal f) -> Right (FltVal (abs f))
+                                     _                -> Left (ExprErr "Abs" (show x ++ " is not a float or an integer"))
+eval vars (Mod x y)           = case (eval vars x, eval vars y) of
+                                     (Right (IntVal a), Right (IntVal b)) -> Right (IntVal (mod a b))
+                                     (Right (IntVal a), Right not_int) -> Left (ExprErr "Mod" (show not_int ++ " is not an integer"))
+                                     (Right (IntVal a), Left eval_err) -> Left eval_err
+                                     (Right not_int, _) -> Left (ExprErr "Mod" (show not_int ++ " is not an integer"))
+                                     (Left eval_err, _) -> Left eval_err
+eval vars expr = case expr of
+  --math
+  Add e e2 -> mathOP vars expr
+  Sub e e2 -> mathOP vars expr
+  Mul e e2 -> mathOP vars expr
+  Div e e2 -> mathOP vars expr
+  Pow e e2 -> mathOP vars expr
+  --compare
+  Eq  e e2 -> boolOp  vars expr
+  NE  e e2 -> boolOp  vars expr
+  Lt  e e2 -> boolOp  vars expr
+  Gt  e e2 -> boolOp  vars expr
+  LE e e2 -> boolOp  vars expr
+  GE e e2 -> boolOp  vars expr
+  --logical operator
+  And e e2 -> andorBoolOp     vars expr
+  Or  e e2 -> andorBoolOp     vars expr
+  Not e    -> notBoolOp       vars expr
+  otherOp  -> Left (ExprErr (show otherOp) ("Unknown operations: " ++ show otherOp))
 
-pCommand :: Parser Command
-pCommand = do t <- letter
-              char '='
-              e <- pExpr
-              return (Set [t] e)
-            ||| do string "print"
-                   space
-                   e <- pExpr
-                   return (Print e)
 
-pExpr :: Parser Expr
-pExpr = do t <- pTerm
-           do char '+'
-              e <- pExpr
-              return (Add t e)
-            ||| do char '-'
-                   e <- pExpr
-                   return (Sub t e)
-                 ||| return t
+--Math operations
+mathOP :: BTree -> Expr -> Either Error Value
+mathOP vars expr = let (func, x, y) = case expr of
+                                           Add expr1 expr2 -> ((+), expr1, expr2)
+                                           Sub expr1 expr2 -> ((-), expr1, expr2)
+                                           Mul expr1 expr2 -> ((*), expr1, expr2)
+                                           Div expr1 expr2 -> ((/), expr1, expr2)
+                                           Pow expr1 expr2 -> ((**), expr1, expr2)
+                       in case (eval vars x, eval vars y) of
+                               (Right (FltVal a), Right (FltVal b)) -> Right (FltVal (func a b))
+                               (Right (FltVal f), Right (IntVal i)) -> Right (FltVal (func f (fromIntegral i)))
+                               (Right (IntVal i), Right (FltVal f)) -> Right (FltVal (func (fromIntegral i) f))
+                               (Right (IntVal a), Right (IntVal b)) -> Right (IntVal (round (func (fromIntegral a) (fromIntegral b))))
+                               (Right (FltVal f), Right not_num)    -> Left (ExprErr "mathOP" (show not_num ++ " is not a number"))
+                               (Right (IntVal i), Right not_num)    -> Left (ExprErr "mathOP" (show not_num ++ " is not a number"))
+                               (Right (FltVal f), Left eval_err)    -> Left eval_err
+                               (Right (IntVal f), Left eval_err)    -> Left eval_err
+                               (Right not_num, _)                   -> Left (ExprErr "mathOP" (show not_num ++ " is not a number"))
+                               (Left eval_err, _)                   -> Left eval_err
 
-pFactor :: Parser Expr
-pFactor = do d <- digit
-             return (Val (digitToInt d))
-           ||| do v <- letter
-                  error "Variables not yet implemented"
-                ||| do char '('
-                       e <- pExpr
-                       char ')'
-                       return e
 
-pTerm :: Parser Expr
-pTerm = do f <- pFactor
-           do char '*'
-              t <- pTerm
-              return (Mult f t)
-            ||| do char '/'
-                   t <- pTerm
-                   return (Div f t)
-                 ||| return f
+boolOp :: BTree -> Expr -> Either Error Value
+boolOp vars expr = let (ordering, x, y) = case expr of
+                                               Lt  expr1 expr2 -> ([LT],  expr1, expr2)
+                                               Gt  expr1 expr2 -> ([GT],  expr1, expr2)
+                                               LE expr1 expr2 -> ([LT, EQ], expr1, expr2)
+                                               GE expr1 expr2 -> ([GT, EQ], expr1, expr2)
+                                               Eq  expr1 expr2 -> ([EQ], expr1, expr2)
+                                               NE  expr1 expr2 -> ([LT, GT], expr1, expr2)
+                       in case (eval vars x, eval vars y) of
+                               (Right (StrVal  a), Right (StrVal  b)) -> Right (BoolVal (compare a b `elem` ordering))
+                               (Right (FltVal  a), Right (FltVal  b)) -> Right (BoolVal (compare a b `elem` ordering))
+                               (Right (FltVal  a), Right (IntVal  b)) -> Right (BoolVal (compare a (fromIntegral b) `elem` ordering))
+                               (Right (IntVal  a), Right (FltVal  b)) -> Right (BoolVal (compare (fromIntegral a) b `elem` ordering))
+                               (Right (IntVal  a), Right (IntVal  b)) -> Right (BoolVal (compare a b `elem` ordering))
+                               (Right (BoolVal a), Right (BoolVal b)) -> Right (BoolVal (compare a b `elem` ordering))
+                               (Right a, Right b) -> Left (ExprErr "boolOp" ("Bool operations between " ++ show x ++ " and " ++ show y ++ " are not supported"))
+                               (Right _, Left eval_err) -> Left eval_err
+                               (Left eval_err, _) -> Left eval_err
 
-pQuit :: Parser Command
-pQuit = do string "quit"
-           return Quit
+notBoolOp :: BTree -> Expr -> Either Error Value
+notBoolOp vars (Not x) = case eval vars x of
+  Right (BoolVal  a) -> Right (BoolVal (not a))
+  Right not_bool -> Left (ExprErr "not" (show not_bool ++ " is not a boolean"))
+  Left eval_err -> Left eval_err
+
+andorBoolOp :: BTree -> Expr -> Either Error Value
+andorBoolOp vars expr = let (func, x, y) = case expr of
+                                                And expr1 expr2 -> ((&&), expr1, expr2)
+                                                Or  expr1 expr2 -> ((||), expr1, expr2)
+                            in case (eval vars x, eval vars y) of
+                                    (Right (BoolVal a), Right (BoolVal b)) -> Right (BoolVal (func a b) )
+                                    (Right a, Right b) -> Left (ExprErr "andorBoolOp" ("Bool operations between " ++ show x ++ " and " ++ show y ++ " are not supported"))
+                                    (Right _, Left eval_err) -> Left eval_err
+                                    (Left eval_err, _) -> Left eval_err
+
--- src/REPL.hs
+++ src/REPL.hs
@@ -1,44 +1,199 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module REPL where
 
+import System.Console.Haskeline
 import Expr
 import Parsing
+import Control.Monad.IO.Class
+import Control.Monad.Trans (lift)
+import Control.Monad.State.Strict (get, put, runStateT, StateT)
 
-data LState = LState { vars :: [(Name, Int)] }
+type StateM = StateT State IO
+type InputM = InputT StateM
+data State = State {vars :: BTree, commands :: [String],
+                    functions :: [(Name, [Name], [Command])],
+                    wordList :: [String]}
 
-initLState :: LState
-initLState = LState []
+initHLCompletionList :: [String]
+initHLCompletionList = ["print", "def", "while", "if", "else", "toFloat(", "toInt(", "toString(", "quit", "True", "False"]
+
+initState :: State
+initState = State Leaf [] [("", [""], [])] initHLCompletionList
 
 -- Given a variable name and a value, return a new set of variables with
 -- that name and value added.
 -- If it already exists, remove the old value
-updateVars :: Name -> Int -> [(Name, Int)] -> [(Name, Int)]
-updateVars = undefined
+updateVars :: Name -> Value -> BTree -> BTree
+updateVars _name _value Leaf = Node (_name, _value) Leaf Leaf
+updateVars _name _value (Node (name, value) ltree rtree)
+  | _name > name = Node (name, value) ltree (updateVars _name _value rtree)
+  | _name < name = Node (name, value) (updateVars _name _value ltree) rtree
+  | otherwise = updateVars name _value (dropVar name (Node (name, value) ltree rtree))
 
 -- Return a new set of variables with the given name removed
-dropVar :: Name -> [(Name, Int)] -> [(Name, Int)]
-dropVar name [] = []
-dropVar name tuples = [a | a <- tuples, fst a /= name]
+dropVar :: Name -> BTree -> BTree
+dropVar _name Leaf = Leaf
+dropVar _name (Node (name, value) ltree rtree)
+  | _name > name = Node (name, value) ltree (dropVar _name rtree)
+  | _name < name = Node (name, value) (dropVar _name ltree) rtree
+  | otherwise = case (ltree, rtree) of
+    (_, Leaf) -> ltree
+    (Leaf, _) -> rtree
+    _ -> let (right_smallest_var, right_smallest_val) = smallestVar rtree
+             smallestVar tree = case tree of
+                                    Node (name_, value_) Leaf _ -> (name_, value_)
+                                    Node (_, _) ltree _ -> smallestVar ltree
+             in Node (right_smallest_var, right_smallest_val) ltree (dropVar right_smallest_var rtree)
 
-process :: LState -> Command -> IO ()
-process st (Set var e) 
-     = do let st' = undefined
-          -- st' should include the variable set to the result of evaluating e
-          repl st'
-process st (Print e) 
-     = do let st' = undefined
-          -- Print the result of evaluation
-          repl st'
+updateFunctions :: Name -> [Name] -> [Command] -> [(Name, [Name], [Command])] -> [(Name, [Name], [Command])]
+updateFunctions name _vars _commands [] = [(name, _vars, _commands)]
+updateFunctions name _vars _commands ((n, v, c): funs)
+  | name == n = (name, _vars, _commands) : funs
+  | otherwise = (n, v, c) : updateFunctions name _vars _commands funs
+
+process :: State -> Command -> InputT StateM State
+process st (Set var e) =
+  do
+    case eval (vars st) e of
+      Left (ExprErr expr err_msg) -> do outputStrLn ("Error on " ++ expr ++ ": " ++ err_msg)
+                                        return st -- error
+      Right Input -> do inpVal <- getInputLine "Input > "
+                        case inpVal of
+                         Just inp -> return (st {vars = updateVars var (StrVal inp) (vars st)})
+                         Nothing -> return (st {vars = updateVars var (StrVal "") (vars st)})
+      Right (FunCall name exprs) -> do val <- funCallVal st name exprs
+                                       case val of
+                                        Right eval_res -> return st {vars = updateVars var eval_res (vars st)}
+                                        Left (ExprErr expr err_msg) -> do outputStrLn ("Error on " ++ expr ++ ": " ++ err_msg)
+                                                                          return st -- error
+      Right eval_res -> do
+        let st' = st {vars = updateVars var eval_res (vars st)}
+        -- st' should include the variable set to the result of evaluating e
+        return st'
+process st (Print e) =
+  do
+    case eval (vars st) e of
+         Left (ExprErr expr err_msg) -> do outputStrLn ("Error on " ++ expr ++ ": " ++ err_msg)
+         Right Input -> do inpVal <- getInputLine "Input > "
+                           case inpVal of
+                                Just inp -> do outputStrLn inp
+                                Nothing -> do outputStrLn ""
+         Right (FunCall name exprs) -> do val <- funCallVal st name exprs
+                                          case val of
+                                            Right eval_res -> outputStrLn (show eval_res) -- how will it show a string
+                                            Left (ExprErr expr err_msg) -> do outputStrLn ("Error on " ++ expr ++ ": " ++ err_msg)
+         Right eval_res -> do
+           outputStrLn (show eval_res)
+    return st
+
+process st (IfE e b1 b2) = case eval (vars st) e of
+  Right (BoolVal True)  -> do processBlock st b1
+  Right (BoolVal False) -> do processBlock st b2
+  _                    -> do outputStrLn "Invalid if conditional"
+                             return st
+process st (If e b1) = case eval (vars st) e of
+  Right (BoolVal True)  -> do processBlock st b1
+  Right (BoolVal False) -> do return st
+  _                     -> do outputStrLn "Invalid if conditional"
+                              return st
+
+process st (While e block) = loop st block e
+  where loop :: State -> [Command] -> Expr -> InputT StateM State
+        loop state cmds expr = case eval (vars state) expr of
+          Right (BoolVal True)  -> do st' <- processBlock state cmds
+                                      loop st' cmds expr
+          Right (BoolVal False) -> return state
+          _                    -> do outputStrLn "Invalid while conditional"
+                                     return state
+process st (Func name vars commands) = return st {functions = updateFunctions name vars commands (functions st)}
+process st (VoidFuncCall name exprs) = case func of
+  [] -> return st
+  [(fname, vnames, commands)] -> if length exprs == length vnames && blockIsVoid commands
+                                    then do sState <- assignVals scopedState vnames exprs
+                                            processBlock sState commands
+                                            return st
+                                 else return st
+  where scopedState :: State
+        scopedState = st
+        func :: [(Name, [Name], [Command])]
+        func = filter (\(x, _, _) -> x == name) (functions st)
+        assignVals :: State -> [Name] -> [Expr] -> InputT StateM State
+        assignVals state (v:vs) (e:es) = do state' <- process state (Set v e)
+                                            assignVals state' vs es
+        assignVals state []     []     = return state
+
+processBlock :: State -> [Command] -> InputT StateM State
+processBlock st [cmd]   = do process st cmd
+processBlock st (cmd: cmds) = do st' <- process st cmd
+                                 processBlock st' cmds
+processBlock st _           = return st
+
+processBlockRet :: (State, Either Error Expr) -> [Command] -> InputT StateM (State, Either Error Expr)
+processBlockRet (st, _) (Return e: _)   = return (st, Right e)
+processBlockRet (st, _) [cmd]   = do st' <- process st cmd
+                                     return (st', Left (ExprErr "Function call" "No return statement"))
+processBlockRet (st, _) (cmd: cmds) = do st' <- process st cmd
+                                         processBlockRet (st', Left (ExprErr "" "")) cmds
+
+blockIsVoid :: [Command] -> Bool
+blockIsVoid []            = True
+blockIsVoid (Return x: _) = False
+blockIsVoid (x: xs)       = blockIsVoid xs
+
+funCallVal :: State -> Name -> [Expr] -> InputT StateM (Either Error Value)
+funCallVal st name exprs = case fun of
+        [] -> return (Left (ExprErr "Function call" "No such function"))
+        [(fname, vnames, commands)] -> if length exprs == length vnames && not(blockIsVoid commands)
+                                          then do sState <- assignVals scopedState vnames exprs
+                                                  (st', e) <- processBlockRet (sState, Left (ExprErr "" "")) commands
+                                                  case e of
+                                                    Right expression -> return (eval (vars st') expression)
+                                                    Left (ExprErr expr err_msg) -> return (Left (ExprErr expr err_msg)) -- Should never happen
+                                       else return (Left (ExprErr "Function call" "Number of argument does not match or there is no return statement"))
+        where scopedState :: State
+              scopedState = st
+              fun :: [(Name, [Name], [Command])]
+              fun = filter (\(x, _, _) -> x == name) (functions st)
+              assignVals :: State -> [Name] -> [Expr] -> InputT StateM State
+              assignVals state (v:vs) (e:es) = do state' <- process state (Set v e)
+                                                  assignVals state' vs es
+              assignVals state []     []     = return state
 
 -- Read, Eval, Print Loop
 -- This reads and parses the input using the pCommand parser, and calls
 -- 'process' to process the command.
 -- 'process' will call 'repl' when done, so the system loops.
 
-repl :: LState -> IO ()
-repl st = do putStr ("> ")
-             inp <- getLine
-             case parse pCommand inp of
-                  [(cmd, "")] -> -- Must parse entire input
-                          process st cmd
-                  _ -> do putStrLn "Parse error"
-                          repl st
+repl :: InputM ()
+repl = do st <- lift get
+          inp <- case commands st of
+            [] -> getInputLine "> "
+            (x:xs) -> do lift $ put st {commands = xs}
+                         return (Just x)
+          st <- lift get
+          case inp of
+            Nothing    -> return ()
+            Just input ->
+                case parse pStatement input of
+                    [(cmd, "")] -> -- Must parse entire input
+                      case cmd of
+                        (Set var e) -> do st' <- process st cmd
+                                          if var `notElem` wordList st'
+                                            then do lift $ put st' {wordList = var : wordList st'}
+                                                    repl
+                                          else do lift $ put st' {wordList = wordList st'}
+                                                  repl
+                        Quit -> return()
+                        (Func name _ _)    -> do st' <- process st cmd
+                                                 if (name ++ "(") `notElem` wordList st
+                                                  then do lift $ put st' {wordList = (name ++ "(") : wordList st'}
+                                                          repl
+                                                 else do lift $ put st' {wordList = wordList st'}
+                                                         repl
+                        (Expr expr) -> do process st (Print expr)
+                                          repl
+                        _ -> do st' <- process st cmd
+                                lift $ put st'
+                                repl
+                    _ -> do outputStrLn "Parse error"
+                            repl
